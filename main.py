f03f72bed5ec570bf15492b4de01fc5a2750864f
Langrzyk <betina.langrzyk@gmail.com>
2020-04-04 20:32:57 +0200

    Zadanie 3

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 # main.py
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+app.counter = 0
+
+class PatientRq(BaseModel):
+    name: str
+    surename: str
+
+class PatientResp(BaseModel):
+    id: int
+    patient: dict
 
 app = FastAPI()
 
@@ -8,18 +18,23 @@ app = FastAPI()
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get("/method")
+@app.get("/method/")
 def method_get():
     return {"method": "GET"}
 
-@app.post("/method")
+@app.post("/method/")
 def method_post():
     return {"method": "POST"}
 
-@app.put("/method")
+@app.put("/method/")
 def method_put():
     return {"method": "PUT"}
 
-@app.delete("/method")
+@app.delete("/method/")
 def method_delete():
     return {"method": "DELETE"}
+
+@app.post("/patient/", response_model=PatientResp)
+def receive_patient(rq: PatientRq):
+    app.counter += 1
+    return PatientResp(id=app.counter,patient=rq.dict())
