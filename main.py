5cf24fbe144f53ea427a13046bf0e06390b1328f
Langrzyk <betina.langrzyk@gmail.com>
2020-04-04 19:21:16 +0200

    Zadanie 2

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method")
+def method_get():
+    return {"method": "GET"}
+
+@app.post("/method")
+def method_post():
+    return {"method": "POST"}
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+@app.delete("/method")
+def method_delete():
+    return {"method": "DELETE"}
