37a827e9b19de551ae245e17cfc86215ce9ceef4
Langrzyk <betina.langrzyk@gmail.com>
2020-05-02 22:30:54 +0200

    Tabela 2.0

--- routers/homework4.py
+++ routers/homework4.py
@@ -1,25 +1,15 @@
 import sqlite3 as sql
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 
 router = APIRouter()
 
 @router.on_event("startup")
 async def startup():
-    router.db_connection = sql.connect('chinook.db')
+    router.db_connection = sql.connect("chinook.db")
 
 @router.on_event("shutdown")
 async def shutdown():
     router.db_connection.close()
-
-@router.get("/test")
-async def tracks():
-    router.db_connection.row_factory = lambda cursor, x: x[0]
-
-    tracks = router.db_connection.execute("SELECT name FROM tracks").fetchall()
-    return {
-        "tracks": tracks,
-    }
-
 
 @router.get("/tracks")
 async def tracks(page: int = 0, per_page: int = 10):
@@ -29,3 +19,15 @@ async def tracks(page: int = 0, per_page: int = 10):
         {'per_page': per_page, 'offset': page*per_page}).fetchall()
 
     return tracks
+
+@router.get("/tracks/composers")
+async def composers(response: Response, composer_name: str):
+    router.db_connection.row_factory = lambda cursor, x: x[0]
+    data = router.db_connection.execute(
+        "SELECT Name FROM tracks WHERE Composer = :composer ORDER By name",
+        {'composer': composer_name}).fetchall()
+    if len(data) == 0:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail":{"error":"No tracks for composer"}}
+
+    return data
