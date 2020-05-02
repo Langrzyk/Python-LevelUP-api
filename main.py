81dd86c46d99f49976c0e72718e1a51c4111338c
Langrzyk <betina.langrzyk@gmail.com>
2020-05-02 21:04:20 +0200

    Tabela 1.1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from routers import homework1, homework3
+from routers import homework1, homework3, homework4
 
 app = FastAPI()
 
 app.include_router(homework1.router, tags=['endpoint homework1'])
 app.include_router(homework3.router, tags=['endpoint homework3'])
+app.include_router(homework4.router, tags=['endpoint homework4'])
---
+++ routers/homework4.py
@@ -0,0 +1,22 @@
+import sqlite3 as sql
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.on_event("startup")
+async def startup():
+    router.db_connection = sql.connect('./chinook.db')
+
+@router.on_event("shutdown")
+async def shutdown():
+    router.db_connection.close()
+
+
+@router.post("/tracks")
+async def tracks(page: int = 0, per_page: int = 10):
+    router.db_connection.row_factory = sql.Row
+    tracks = router.db_connection.execute(
+        "SELECT * FROM tracks LIMIT :per_page OFFSET :offset",
+        {'per_page': per_page, 'offset': page*per_page}).fetchone()
+
+    return tracks,
