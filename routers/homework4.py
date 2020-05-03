a4cc34dbba7b6df09b79c715cb4d8963fd2d259e
Langrzyk <betina.langrzyk@gmail.com>
2020-05-03 15:11:10 +0200

    Tabela 4.0

--- routers/homework4.py
+++ routers/homework4.py
@@ -1,10 +1,20 @@
 import sqlite3 as sql
 from fastapi import APIRouter, Response, status
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 class Albums(BaseModel):
     title: str
     artist_id: int
+
+class Customers(BaseModel):
+    company: str
+    address: str
+    city: str
+    state: str
+    country: str
+    postalcode: str
+    fax: str
 
 router = APIRouter()
 
@@ -43,7 +53,6 @@ async def album_add(response: Response, album: Albums):
     artist = router.db_connection.execute(
         "SELECT Artistid FROM artists WHERE Artistid = :id",
         {'id': album.artist_id}).fetchall()
-    print(1)
     if not artist:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"detail":{"error":"No Artist with id"}}
@@ -57,13 +66,37 @@ async def album_add(response: Response, album: Albums):
         "SELECT albumid, title, artistid FROM albums WHERE albumid = :id;",
         {'id': new_album_id }).fetchone()
     response.status_code = status.HTTP_201_CREATED
-    
+
     return album
 
 @router.get("/albums/{album_id}")
 async def tracks_composers(response: Response, album_id: int):
 	router.db_connection.row_factory = sql.Row
-	album = router.db_connection.execute("SELECT * FROM albums WHERE AlbumId = ?",
-		(album_id, )).fetchone()
+	album = router.db_connection.execute("SELECT * FROM albums WHERE AlbumId = :id",
+		{'id': album_id}).fetchone()
 
 	return album
+
+
+@router.put("/customers/{customer_id}")
+async def actual_customer(response: Response, customer: Customers, customer_id: int):
+    router.db_connection.row_factory = sql.Row
+    data = router.db_connection.execute("SELECT CustomerId FROM customers WHERE CustomerId = :id",
+    {'id':customer_id}).fetchone()
+    if not data:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail":{"error":"No client with id"}}
+
+    update_customer = jsonable_encoder(customer)
+    changes = ", ".join(f"{key} = '{update_customer[key]}'"
+        for key in update_customer if update_customer[key] != 'string')
+    updates = router.db_connection.execute(
+        f"UPDATE customers SET {changes} WHERE CustomerId = :id",
+        {'id': customer_id})
+    router.db_connection.commit()
+
+    router.db_connection.row_factory = sql.Row
+    custome = router.db_connection.execute("SELECT * FROM customers WHERE CustomerId = :id",
+        {'id':customer_id}).fetchone()
+
+    return custome
